02aac6e18440bae4e2bf41ba008d6d0baf7e0734
chandira_ekkanayaka <chandiraems@gmail.com>
2023-06-28 00:24:45 +0530

    categories loaded from the database

--- goid-html/category.php
+++ goid-html/category.php
@@ -1,32 +1,64 @@
+<?php
+include('../goid-html/functions/userfunctions.php');
+
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Food Menu</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="categorycss.css">
-    <!-- fontawesome icon -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-  </head>
-  <body>
 
-    <section class = "menu">
-      <div class = "menu-container">
-        <div class = "menu-head">
-          <h2>Foods & Drinks Menu</h2>
-          <p>Food is any substance consumed to provide nutritional support for an organism. A drink or beverage is a liquid intended for human consumption</p>
-        </div>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Food Menu</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="categorycss.css">
+  <!-- fontawesome icon -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+</head>
 
-        <div class = "menu-btns">
-          <button type = "button" class = "menu-btn active-btn" id = "btnRice">Fride rice</button>
-          <button type = "button" class = "menu-btn" id = "btnKottu">Kottu</button>
-          <button type = "button" class = "menu-btn" id = "btnbakery">Bakery items</button>
-          <button type = "button" class =  "menu-btn" id = "btnDrinks">Drinks</button>
-        </div>
+<body>
 
-        <!--
+  <section class="menu">
+    <div class="menu-container">
+      <div class="menu-head">
+        <h2>Foods & Drinks Menu</h2>
+        <p>Food is any substance consumed to provide nutritional support for an organism. A drink or beverage is a liquid intended for human consumption</p>
+      </div>
+
+      <div class="menu-btns">
+
+        <?php
+
+        $categories = getAllActive('categories');
+
+        if (mysqli_num_rows($categories) > 0) {
+
+          foreach ($categories as $items) {
+
+        ?>
+
+            <button type="button" class="menu-btn"><?= $items['cname']; ?></button>
+
+        <?php
+
+          }
+        }
+
+        ?>
+
+       
+
+      </div>
+
+
+      <!--<button type="button" class="menu-btn active-btn" id="btnRice">Fride rice</button>
+        <button type="button" class="menu-btn" id="btnKottu">Kottu</button>
+        <button type="button" class="menu-btn" id="btnbakery">Bakery items</button>
+        <button type="button" class="menu-btn" id="btnDrinks">Drinks</button>-->
+
+
+      <!--
 
           <div class = "food-items">
          
@@ -444,8 +476,9 @@
 
 
         -->
-    
 
-    <script src="script.js"></script>
-  </body>
+
+      <script src="script.js"></script>
+</body>
+
 </html>
--- goid-html/functions/userfunctions.php
+++ goid-html/functions/userfunctions.php
@@ -1,9 +1,11 @@
 <?php 
-    include('../config/connection.php');
+    include('../goid-html/config/connection.php');
 
 
     function getAllActive($table){
-        
+        global $con;
+        $query = "select * from $table where status='active'";
+        return $query_run = mysqli_query($con,$query);
 
     }
 
