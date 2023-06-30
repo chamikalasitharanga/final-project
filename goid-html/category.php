0a755cdb7127d4a7b0bc92a159b2d8477d37fa6b
chandira_ekkanayaka <chandiraems@gmail.com>
2023-06-30 08:20:46 +0530

    color problem solved

--- goid-html/category.php
+++ goid-html/category.php
@@ -15,8 +15,6 @@ include_once('../goid-html/functions/userfunctions.php');
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="categorycss.css">
   <!-- fontawesome icon -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
 </head>
 
@@ -57,10 +55,15 @@ include_once('../goid-html/functions/userfunctions.php');
       </div>
     </div>
   </section>
-  <?php
   
-      include 'food.php';
-    
+  <?php 
+  
+        if(isset($_GET['category'])){
+
+          include_once 'food.php';
+
+        }
+
   ?>
   <script src="script.js"></script>
 </body>
